493a4b726434dd4fb324312f812c3c769b76666f
HauptmanDev <MainVados@gmail.com>
2020-05-13 21:33:41 +0300

    Started new section. Changed todoState and todoReducer.

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -3,18 +3,29 @@ import {Alert} from 'react-native'
 
 import {TodoContext} from "./todoContext";
 import {todoReducer} from "./todoReducer";
-import {ADD_TODO, REMOVE_TODO, UPDATE_TODO} from "../types";
+import {ADD_TODO, CLEAR_ERROR, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO} from "../types";
 import {ScreenContext} from "../screen/screenContext";
 
 export const TodoState = ({children}) => {
-    const initialState = {todos: []};
+    const initialState = {
+        todos: [],
+        loading: false,
+        error: null,
+    };
     const {changeScreen} = useContext(ScreenContext);
     const [state, dispatch] = useReducer(todoReducer, initialState);
-    const addTodo = title => {
-        dispatch({type: ADD_TODO, title})
+    const addTodo = async title => {
+        const response = await fetch('https://rn-todo-app-9ba3c.firebaseio.com/todos.json', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({title})
+        });
+        const data = await response.json();
+        console.log('DATA', data.name);
+        dispatch({type: ADD_TODO, title, id: data.name})
     };
     const removeTodo = id => {
-        const todo = state.todos.find(el=> el.id === id);
+        const todo = state.todos.find(el => el.id === id);
         Alert.alert(
             'Удаление элемента',
             `Хотите удалить ${todo.title}?`,
@@ -33,6 +44,18 @@ export const TodoState = ({children}) => {
     };
     const updateTodo = (id, title) => {
         dispatch({type: UPDATE_TODO, id, title})
+    };
+    const showLoader = () => {
+        dispatch({type: SHOW_LOADER})
+    };
+    const hideLoader = () => {
+        dispatch({type: HIDE_LOADER})
+    };
+    const showError = error => {
+        dispatch({type: SHOW_ERROR, error})
+    };
+    const clearError = () => {
+        dispatch({type: CLEAR_ERROR})
     };
     return (
         <TodoContext.Provider value={{
--- src/context/types.js
+++ src/context/types.js
@@ -1,5 +1,11 @@
-export const ADD_TODO= 'ADD-TODO';
-export const REMOVE_TODO= 'REMOVE-TODO';
-export const UPDATE_TODO= 'UPDATE-TODO';
-export const CHANGE_SCREEN= 'CHANGE-SCREEN';
+export const ADD_TODO = 'ADD-TODO';
+export const REMOVE_TODO = 'REMOVE-TODO';
+export const UPDATE_TODO = 'UPDATE-TODO';
+export const CHANGE_SCREEN = 'CHANGE-SCREEN';
+export const SHOW_LOADER = 'SHOW-LOADER';
+export const HIDE_LOADER = 'HIDE-LOADER';
+export const SHOW_ERROR = 'SHOW-ERROR';
+export const CLEAR_ERROR = 'CLEAR-ERROR';
+export const FETCH_TODOS = 'FETCH-TODOS';
+
 
